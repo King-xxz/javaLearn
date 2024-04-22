c579ef1a153d9d2575959ec3e248a052c0b014d6
King-xxz <1853091038@qq.com>
2024-04-22 20:04:59 +0800

    Add files via upload

---
+++ test/src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int a=10;
+        System.out.println(a);
+        for(a=0;a<10;a++){
+            System.out.println(a);
+        }
+    }
+}
